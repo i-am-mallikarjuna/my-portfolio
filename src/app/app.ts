322a32f3ec93c4f23ac03e95deb96398acf17310
Arjun-m-99 <67136691+Arjun-m-99@users.noreply.github.com>
2026-08-09 16:03:33 +0530

    added log sheet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,38 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./shared/navbar/navbar";
-import { MiniProjects } from './features/mini-projects/mini-projects';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Navbar, RouterOutlet, MiniProjects],
+  imports: [Navbar, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+  ngOnInit() {
+    this.logVisitorData();
+  }
+
+  logVisitorData() {
+    // 1. Fetch the user's IP and Location from a free public API
+    fetch('https://ipapi.co/json/')
+      .then(response => response.json())
+      .then(locationData => {
+
+        // 2. Send that data to your Google Sheet Web App
+        const googleScriptUrl = 'https://script.google.com/macros/s/AKfycbzKpuTnVT1O0pb6gz2wiXdVuhvBO-GvhlwG2Kh2EAc5qrSA4G3Ue1MHxnGMoalUuFqNhg/exec';
+
+        fetch(googleScriptUrl, {
+          method: 'POST',
+          mode: 'no-cors', // Bypasses browser security warnings for background tracking
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(locationData)
+        });
+
+      })
+      .catch(error => console.error('Tracking blocked or failed', error));
+  }
+
   protected readonly title = signal('MyPortfolio-UI');
 }
