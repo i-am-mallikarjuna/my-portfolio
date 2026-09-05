82a1849d46043ded6f80b57ad49ffb1e4e83f126
i-am-mallikarjuna <mallikarjun.akunuri1999@gmail.com>
2026-09-05 17:04:44 +0530

    fixed a couple of minor pjs

--- src/app/features/mini-projects/mini-projects.ts
+++ src/app/features/mini-projects/mini-projects.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-mini-projects',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatFormFieldModule, MatSelectModule],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatSelectModule, MatIconModule],
   templateUrl: './mini-projects.html',
   styleUrl: './mini-projects.scss',
 })
@@ -19,7 +20,7 @@ export class MiniProjects {
   // List of your HTML files. Update these exact filenames if needed!
   experiments = [
     { name: 'Beating Heart', path: 'mini-projects/beating-heart.html' },
-    { name: 'Button Bear', path: 'mini-projects/button-bear.html' },
+    // { name: 'Button Bear', path: 'mini-projects/button-bear.html' },
     { name: 'Funny Black Holes', path: 'mini-projects/FunnyBlackHoles.html' },
     { name: 'Glowing Text', path: 'mini-projects/glowingText.html' },
     { name: 'Digital Wish Tree', path: 'mini-projects/DigitalWishTree.html' },
@@ -27,16 +28,20 @@ export class MiniProjects {
     { name: 'Interactive Book', path: 'mini-projects/Book.html' },
     { name: 'Circle Lines', path: 'mini-projects/circleLines.html' },
     { name: 'HBD Tuffy', path: 'mini-projects/HappyBirthday_Tuffy.html' },
-    { name: 'HBD Tuffy (Alt)', path: 'mini-projects/HappyBirthdayTufffy .html' }
+    // { name: 'HBD Tuffy (Alt)', path: 'mini-projects/HappyBirthdayTufffy .html' }
   ];
 
   // Set the default project to load first (sanitized!)
   selectedUrl: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.experiments[0].path);
   activeProjectName: string = this.experiments[0].name;
+  selectedPath: string = this.experiments[0].path;
+  selectedExperiment = this.experiments[0];
 
   // Function to change the iframe source when a button is clicked
   selectExperiment(project: any) {
     this.selectedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(project.path);
     this.activeProjectName = project.name;
+    this.selectedPath = project.path;
+    this.selectedExperiment = project;
   }
 }
